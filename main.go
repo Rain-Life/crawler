e7c4fe1d9d166d9e4f766e2ab8cbdf23a2f1100f
陈兴生 <chenxingsheng@51shebao.com>
2020-10-20 09:53:45 +0800

    2020.10.20

--- main.go
+++ main.go
@@ -1,57 +1,13 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"golang.org/x/net/html/charset"
-	"golang.org/x/text/encoding"
-	"golang.org/x/text/transform"
-	"io"
-	"io/ioutil"
-	"net/http"
-	"regexp"
+	"crawler/engine"
+	"crawler/zhenai/parser"
 )
 
 func main() {
-	resp, err := http.Get("http://www.zhenai.com/zhenghun")
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		fmt.Println("Error：status code ", resp.StatusCode)
-		return
-	}
-
-	//处理网页编码的问题
-	e := determineEncoding(resp.Body)
-	utf8Reader := transform.NewReader(resp.Body, e.NewDecoder())
-
-	all, err := ioutil.ReadAll(utf8Reader)
-	if err != nil {
-		panic(err)
-	}
-
-	printCityList(all)
-
-}
-
-func determineEncoding(r io.Reader) encoding.Encoding {
-	bytes, err := bufio.NewReader(r).Peek(1024)
-	if err != nil {
-		panic(err)
-	}
-	e, _, _ := charset.DetermineEncoding(bytes, "")
-
-	return e
-}
-
-func printCityList(contents []byte) {
-	re := regexp.MustCompile(`<a href="(http://www.zhenai.com/zhenghun/[a-z0-9]+)"[^>]*>([^<]+)</a>`)//^非的意思
-	matches := re.FindAllSubmatch(contents, -1)
-	for _, m := range matches {
-		fmt.Printf("City：%s, URL：%s\n", m[2], m[1])
-	}
-	fmt.Printf("Matches found: %d\n", len(matches))
+	engine.Run(engine.Request{
+		Url: "http://www.zhenai.com/zhenghun",
+		ParserFunc: parser.ParseCityList,
+	})
 }
---
+++ zhenai/parser/city.go
@@ -0,0 +1,24 @@
+package parser
+
+import (
+	"crawler/engine"
+	"regexp"
+)
+
+const cityRe = `<a href="(http://album.zhenai.com/u/[0-9]+)"[^>]*>([^<]+)</a>`
+
+func ParseCity(contents []byte) engine.ParseResult {
+	re := regexp.MustCompile(cityRe)//^非的意思
+	matches := re.FindAllSubmatch(contents, -1)
+
+	result := engine.ParseResult{}
+	for _, m := range matches {
+		result.Items = append(result.Items, "User " + string(m[2]))
+		result.Requests = append(result.Requests, engine.Request{
+			Url: string(m[1]),
+			ParserFunc: engine.NilParser,
+		})
+	}
+
+	return result
+}
