e85532bcd9847c5d5c2143dea0c3cf97ccc00ab3
陈兴生 <chenxingsheng@51shebao.com>
2020-10-25 16:55:08 +0800

    rpc

---
+++ learnRPC/server/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	rpcdemo "crawler/learnRPC"
+	"log"
+	"net"
+	"net/rpc"
+	"net/rpc/jsonrpc"
+)
+
+func main() {
+	err := rpc.Register(rpcdemo.DemoService{})
+	if err != nil {
+		panic(err)
+	}
+
+	listener, err := net.Listen("tcp", ":1234")
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Printf("accept error: %v", err)
+			continue
+		}
+
+		go jsonrpc.ServeConn(conn)
+	}
+}
